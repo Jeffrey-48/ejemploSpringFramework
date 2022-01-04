e4e78878784c0bc9a4cd27499b677103946e40b8
Jeffrey portatil <javargasg_1@uqvirtual.edu.co>
2022-01-04 16:53:14 -0500

    inyeccion de campos desde xml

--- src/inversion_of_control/SecretarioEmpleado.java
+++ src/inversion_of_control/SecretarioEmpleado.java
@@ -3,12 +3,8 @@ package inversion_of_control;
 public class SecretarioEmpleado implements Empleado{
 	
 	private CreacionInforme informeNuevo;
-	
-	
-	//Creacion de constructor que inyecta la dependencia
-	public SecretarioEmpleado(CreacionInforme informeNuevo) {
-		this.informeNuevo = informeNuevo;
-	}
+	private String nombreEmpresa;
+	private String email;
 	
 	@Override
 	public String getTareas() {
@@ -19,6 +15,26 @@ public class SecretarioEmpleado implements Empleado{
 	public String getInforme() {
 		// TODO Auto-generated method stub
 		return "Informe creado por secretario: " + informeNuevo.getInforme();
+	}
+
+	public void setInformeNuevo(CreacionInforme informeNuevo) {
+		this.informeNuevo = informeNuevo;
+	}
+
+	public String getNombreEmpresa() {
+		return nombreEmpresa;
+	}
+
+	public void setNombreEmpresa(String nombreEmpresa) {
+		this.nombreEmpresa = nombreEmpresa;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 }
--- src/inversion_of_control/UsoEmpleado.java
+++ src/inversion_of_control/UsoEmpleado.java
@@ -7,8 +7,15 @@ public class UsoEmpleado {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext contexto = new ClassPathXmlApplicationContext("applicationContext.xml");
 		Empleado juan = contexto.getBean("miEmpleado", Empleado.class);
+		System.out.println("Inyeccion por constructor");
 		System.out.println(juan.getTareas());
 		System.out.println(juan.getInforme());
+		System.out.println("Inyeccion por setter");
+		SecretarioEmpleado carlos = contexto.getBean("miSecretarioEmpleado", SecretarioEmpleado.class);
+		System.out.println(carlos.getTareas());
+		System.out.println(carlos.getInforme());
+		System.out.println(carlos.getEmail());
+		System.out.println(carlos.getNombreEmpresa());
 		contexto.close();
 	}
 
