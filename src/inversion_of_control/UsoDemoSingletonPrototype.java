e463b346de619c4a3e5590e0a5330f9e0288074f
Jeffrey portatil <javargasg_1@uqvirtual.edu.co>
2022-01-05 11:07:17 -0500

    Singleton and Prototype

--- src/inversion_of_control/UsoDemoSingletonPrototype.java
+++ src/inversion_of_control/UsoDemoSingletonPrototype.java
@@ -1,10 +1,23 @@
 package inversion_of_control;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class UsoDemoSingletonPrototype {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//carga de xml
+		ClassPathXmlApplicationContext contexto = new ClassPathXmlApplicationContext("applicationContext2.xml");
+		//peticion de beans al contenedor spring 
+		SecretarioEmpleado maria = contexto.getBean("miSecretarioEmpleado", SecretarioEmpleado.class);
+		SecretarioEmpleado pedro = contexto.getBean("miSecretarioEmpleado", SecretarioEmpleado.class);
+		//imprimimos los objetos y vemos que el espacio de memoria es el mismo
+		System.out.println(maria);
+		System.out.println(pedro);
+		if (maria==pedro) {
+			System.out.println("Apuntan al mismo objeto");
+		}else {
+			System.out.println("No apuntan a el mismo objeto");
+		}
 	}
 
 }
